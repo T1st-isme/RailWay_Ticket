1030b043da00a73c247c2c360953559247234498
Hà Trung Triển <trientrung102@gmail.com>
2023-03-30 11:38:03 +0700

    Create DBHelper, login and register successful, insert user account to DB.

--- app/src/main/java/com/example/railwayticket/ui/LoginActivity.java
+++ app/src/main/java/com/example/railwayticket/ui/LoginActivity.java
@@ -6,35 +6,58 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.railwayticket.DBHelper;
 import com.example.railwayticket.HomeActivity;
 import com.example.railwayticket.R;
 
 public class LoginActivity extends AppCompatActivity {
     Button btLog;
     TextView reg;
+    EditText edUsername, edPassword;
+    DBHelper DB;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btLog = (Button) findViewById(R.id.btLogin);
-        reg =(TextView) findViewById(R.id.tvReg);
+
+        anhxa();
+
+        DB = new DBHelper(this);
+
         btLog.setOnClickListener(view -> {
-            Toast.makeText(this,"Đăng nhập thành công", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-            startActivity(intent);
-            finish();
+            String name = edUsername.getText().toString().trim();
+            String password = edPassword.getText().toString().trim();
+            if (name.equals("") || password.equals("")) {
+                Toast.makeText(this, "Vui lòng điền đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Boolean checkuserpass = DB.checkUserPass(name, password);
+                if (checkuserpass){
+                    Toast.makeText(this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(this,HomeActivity.class);
+                    startActivity(i);
+                }else {
+                    Toast.makeText(this, "Tài khoản hoặc mật khẩu không đúng!", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
-        reg.setOnClickListener(view ->{
+
+        reg.setOnClickListener(view -> {
             Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
             startActivity(intent);
         });
+    }
 
-
-
+    private void anhxa() {
+        btLog = (Button) findViewById(R.id.btLogin);
+        reg = (TextView) findViewById(R.id.tvReg);
+        edUsername = (EditText) findViewById(R.id.edUsername);
+        edPassword = (EditText) findViewById(R.id.edPassword);
     }
 }
--- app/src/main/java/com/example/railwayticket/ui/RegisterActivity.java
+++ app/src/main/java/com/example/railwayticket/ui/RegisterActivity.java
@@ -7,14 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.railwayticket.DBHelper;
 import com.example.railwayticket.R;
 
 public class RegisterActivity extends AppCompatActivity {
     Button btnRegister;
+    EditText edUsername, edPassword, edCPassword, edEmail;
+    DBHelper DB;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -22,12 +27,50 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        anhxa();
+
+        DB = new DBHelper(this);
+
+        btnRegister.setOnClickListener(view -> Register());
+    }
+
+    private void Register() {
+        String name = edUsername.getText().toString().trim();
+        String password = edPassword.getText().toString().trim();
+        String cpassword = edCPassword.getText().toString().trim();
+        String email = edEmail.getText().toString().trim();
+        if (name.equals("") || password.equals("") || cpassword.equals("") || email.equals("")) {
+            Toast.makeText(this, "Vui lòng điền đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+        } else {
+            if (password.equals(cpassword)) {
+                Boolean checkUser = DB.checkUsername(name);
+                if (!checkUser) {
+                    Boolean insert = DB.insertData(name, password);
+                    if (insert) {
+                        Toast.makeText(this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(this, "Đăng ký không thành công", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Toast.makeText(this, "Tài khoản đã được đăng ký! Vui lòng đăng nhập.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else {
+                Toast.makeText(this, "Mật khẩu không khớp!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    private void anhxa() {
         btnRegister = (Button) findViewById(R.id.btRegister);
-        btnRegister.setOnClickListener(view -> {
-            Toast.makeText(this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
-        });
+        edUsername = (EditText) findViewById(R.id.edUsername);
+        edPassword = (EditText) findViewById(R.id.edPassword);
+        edEmail = (EditText) findViewById(R.id.edEmail);
+        edCPassword = (EditText) findViewById(R.id.edConfirmPassword);
     }
 }
